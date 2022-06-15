1a1488b27f273adac1e2d57026ce01c985fd9a89
Dhevano Gading <dhevanogading@gmail.com>
2022-06-15 10:33:17 +0700

    update commit 1.1

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -15,11 +15,10 @@
             <td>{{ $value->tanggal_lahir }}</td>
             <td><a href="{{ url('customer/'.$value->id.'/edit') }}" class="btn btn-primary">Edit</a></td>
             <td>
-              <form action="{{ url('customer/'.$value->id) }}" method="POST">
-                @csrf
+              <x-form action="{{ url('customer/'.$value->id) }}" method="POST">
                 <input type="hidden" name="_method" value="DELETE">
                 <button class="btn btn-danger" type="submit">Delete</button>
-              </form>
+              </x-form>
             </td>
         </tr>
     @endforeach
